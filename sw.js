cc7b11dff61c807b0ad7b7fd828c85fe71b887d9
sideivid <108440943+sideivid@users.noreply.github.com>
2022-06-29 20:25:40 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_ELEMENTS = [
+  "./",
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./style.css",
+  "./components/Contador.js",
+];
+
+const CACHE_NAME = "v1_cache_contador_react";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache
+        .addAll(CACHE_ELEMENTS)
+        .then(() => {
+          self.skipWaiting();
+        })
+        .catch(console.log);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+
+      return fetch(e.request);
+    })
+  );
+});
